4a524059dcb853bccf041264fcc9c1806e8c691e
toro_ponz <toro-ponz@users.noreply.github.com>
2023-03-16 00:46:54 +0900

    add openai bot.

---
+++ openai/openai.py
@@ -0,0 +1,246 @@
+import os
+import textwrap
+
+import openai
+
+from discord import Client, MemberCacheFlags, Intents, Status
+from discord.ext import tasks
+
+from utils import Logger, DateTime
+
+# openai.api_key = os.environ.get('OPENAI_API_KEY')
+
+"""
+OpenAI is a discord bot that based by GPT.
+"""
+class OpenAI(Client):
+
+    """
+    chat history per guild.
+    """
+    chat_histories = {}
+
+    """
+    hour that reset chat histories.
+    """
+    history_reset_hour = 6
+
+    """
+    constructor.
+
+    @param token string (required)discord token.
+    @param logger Logger (optional)utils.Logger instance.
+    """
+    def __init__(self, token, logger=None):
+        super().__init__(
+            intents = Intents.all(),
+            member_cache_flags = MemberCacheFlags.all()
+        )
+
+        if (token is None):
+            raise Exception('token is required.')
+
+        self.token = token
+        self.logger = logger
+
+        if (self.logger is None):
+            self.logger = Logger(os.environ.get('LOG_LEVEL', 'INFO'))
+
+        self.logger.info('Application starting.')
+        self.run(self.token)
+
+    """
+    exec when launched a bot.
+    """
+    async def on_ready(self):
+        self.logger.debug('on_ready')
+
+        await self.change_presence(status=Status.online)
+
+        self.watch.start()
+
+    """
+    exec when received an message.
+
+    @param message discord.Message
+    """
+    async def on_message(self, message):
+        self.logger.debug('on_message')
+
+        if (self.find_user_from_list(message.mentions, self.user.name) is None):
+            if (self.find_role_from_list(message.role_mentions, self.user.name) is None):
+                return
+
+        if (message.author.bot):
+            return
+        
+        await self.command(message)
+
+    """
+    exec command.
+
+    @param message discord.Message
+    """
+    async def command(self, message):
+        commands = message.content.split(' ')
+        guild = message.guild
+        channel = message.channel
+
+        if (commands is None or len(commands) < 2):
+            await self.do_help(channel)
+            return
+
+        if (commands[1] == 'system'):
+            await self.do_system(guild, channel, message.content.split(' ', 2)[2])
+            return
+        
+        if (commands[1] == 'history'):
+            await self.do_history(guild, channel)
+            return
+        
+        if (commands[1] == 'reset'):
+            await self.do_reset_history(guild, channel)
+            return
+        
+        if (commands[1] == 'help'):
+            await self.do_help(channel)
+            return
+
+        await self.do_user(guild, channel, message.content.split(' ', 1)[1])
+
+    """
+    loop tasks.
+    """
+    @tasks.loop(hours=1)
+    async def watch(self):
+        now = DateTime.now()
+        self.logger.debug('started watch at %s.' % (now))
+
+        if (now.hour == self.history_reset_hour):
+            for guild in self.guilds:
+                self.do_reset_history(guild)
+
+    """
+    send chat message to openai.
+
+    @param guild discord.Guild
+    @param role string role of openai message.
+    @param content string content of openai message.
+    """
+    def do_openai(self, guild, role, content):
+        messages = []
+        self.logger.debug(messages)
+
+        if (self.chat_histories.get(guild.id) is not None):
+            messages = self.chat_histories.get(guild.id)
+            self.logger.debug(messages)
+
+        messages.append({'role': role, 'content': content})
+        self.logger.debug(messages)
+
+        reply = 'This command is not implemented yet, dummy reply! Please send api key for openai to @toro_ponz :)'
+        # response = openai.ChatCompletion.create(
+        #     model='gpt-3.5-turbo',
+        #     messages=messages,
+        # )
+        # reply = response.choices[0]['message']['content'].strip()
+
+        messages.append({'role': 'assistant', 'content': reply})
+        self.chat_histories[guild.id] = messages
+
+        return reply
+
+    """
+    send user message to openai.
+
+    @param guild discord.Guild
+    @param channel discord.Channel
+    @param text string content of chat message.
+    """
+    async def do_user(self, guild, channel, text):
+        reply = self.do_openai(guild, 'user', text)
+        await channel.send(reply)
+
+    """
+    send system message to openai.
+
+    @param guild discord.Guild
+    @param channel discord.Channel
+    @param text string content of chat message.
+    """
+    async def do_system(self, guild, channel, text):
+        reply = self.do_openai(guild, 'system', text)
+        await channel.send(reply)
+
+    """
+    get chat histories.
+
+    @param guild discord.Guild
+    @param channel discord.Channel
+    """
+    async def do_history(self, guild, channel):
+        if (self.chat_histories.get(guild.id) is None):
+            await channel.send('histories are empty.')
+            return
+
+        await channel.send('%s' % (self.chat_histories.get(guild.id)))
+
+    """
+    reset chat histories.
+
+    @param guild discord.Guild
+    @param channel discord.Channel
+    """
+    async def do_reset_history(self, guild, channel):
+        self.chat_histories[guild.id] = {}
+        self.logger.info('reset chat histories for %s.' % (guild.name))
+
+        if (channel is not None):
+            await channel.send('reset history.')
+
+    """
+    help.
+
+    @param channel discord.Channel
+    """
+    async def do_help(self, channel):
+        text = textwrap.dedent("""
+        ```
+        usage: @openai <command> [<args>]
+        ---
+        <message>         send user message(role=user).
+        system <message>  send system message(role=system).
+        history           get chat history.
+        reset             reset chat history.
+        help              list available commands and some.
+        ```
+        """).strip()
+
+        await channel.send(text)
+
+    """
+    find user by user name from user list.
+
+    @param users list[discord.User]
+    @param name string search user name.
+    @return discord.User or None
+    """
+    def find_user_from_list(self, users, name):
+        for user in users:
+            if (user.name == name):
+                return user
+
+    """
+    find role by role name from role list.
+
+    @param roles list[discord.Role]
+    @param name string search role name.
+    @return discord.Role or None
+    """
+    def find_role_from_list(self, roles, name):
+        for role in roles:
+            if (role.name == name):
+                return role
+
+
+client = OpenAI(os.environ.get('TOKEN', None))
---
+++ openai/utils.py
@@ -0,0 +1,151 @@
+import os
+
+from datetime import datetime
+from pytz import timezone
+
+LOGGING_LEVEL_DEBUG = 'DEBUG'
+LOGGING_LEVEL_INFO  = 'INFO'
+LOGGING_LEVEL_ERROR = 'ERROR'
+LOGGING_LEVEL_FATAL = 'FATAL'
+LOGGING_LEVEL_NONE  = 'NONE'
+
+TZ = os.getenv('TZ')
+
+"""
+logging class on discord.
+"""
+class Logger():
+    log_level_list = {
+        LOGGING_LEVEL_DEBUG,
+        LOGGING_LEVEL_INFO,
+        LOGGING_LEVEL_ERROR,
+        LOGGING_LEVEL_FATAL,
+        LOGGING_LEVEL_NONE
+    }
+
+    """
+    constructor.
+
+    @param log_level string (required)logging level(DEBUG, INFO, ERROR, FATAL, NONE).
+    """
+    def __init__(self, log_level = LOGGING_LEVEL_INFO):
+
+        if (log_level not in self.__class__.log_level_list):
+            raise Exception('invalid log level.')
+
+        self.log_level = log_level
+
+    """
+    output log on DEBUG level.
+    """
+    def debug(self, text):
+        if (not self.can_logging_debug()):
+            return
+
+        print('[DEBUG] %s' % (text), flush=True)
+
+    """
+    output log on INFO level.
+    """
+    def info(self, text):
+        if (not self.can_logging_info()):
+            return
+
+        print('[INFO] %s' % (text), flush=True)
+
+    """
+    output log on ERROR level.
+    """
+    def error(self, text):
+        if (not self.can_logging_error()):
+            return
+
+        print('[ERROR] %s' % (text), flush=True)
+
+    """
+    output log on FATAL level.
+    """
+    def fatal(self, text):
+        if (not self.can_logging_fatal()):
+            return
+
+        print('[FATAL] %s' % (text), flush=True)
+    
+    """
+    return can logging debug level message.
+
+    @return bool
+    """
+    def can_logging_debug(self):
+        return self.can_logging({
+            LOGGING_LEVEL_DEBUG
+        })
+    
+    """
+    return can logging info level message.
+
+    @return bool
+    """
+    def can_logging_info(self):
+        return self.can_logging({
+            LOGGING_LEVEL_DEBUG,
+            LOGGING_LEVEL_INFO
+        })
+    
+    """
+    return can logging error level message.
+
+    @return bool
+    """
+    def can_logging_error(self):
+        return self.can_logging({
+            LOGGING_LEVEL_DEBUG,
+            LOGGING_LEVEL_INFO,
+            LOGGING_LEVEL_ERROR
+        })
+    
+    """
+    return can logging fatal level message.
+
+    @return bool
+    """
+    def can_logging_fatal(self):
+        return self.can_logging({
+            LOGGING_LEVEL_DEBUG,
+            LOGGING_LEVEL_INFO,
+            LOGGING_LEVEL_ERROR,
+            LOGGING_LEVEL_FATAL
+        })
+    
+    """
+    return can logging none level message.
+
+    @return bool
+    """
+    def can_logging_none(self):
+        return self.can_logging({})
+
+    """
+    return can logging message from level list.
+
+    @param can_logging_log_level_list array[string] approve logging log level list.
+    @return bool
+    """
+    def can_logging(self, can_logging_log_level_list):
+        if (self.log_level in can_logging_log_level_list):
+            return True
+        
+        return False
+
+"""
+datetime warrper class.
+"""
+class DateTime():
+    """
+    return now datetime.
+
+    @return datetime.datetime
+    """
+    @classmethod
+    def now(cls):
+        return datetime.now(tz=timezone(TZ)).replace(microsecond=0)
